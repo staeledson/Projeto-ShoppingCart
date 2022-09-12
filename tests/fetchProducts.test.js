8743fbb8f3fa1e586f6897bf8a79575df8bd819b
Stael Edson <edsonstael@gmail.com>
2022-09-12 14:44:39 -0300

    começando o projeto

--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -1,8 +1,10 @@
 require('../mocks/fetchSimulator');
+const { expect } = require('chai');
 const { fetchProducts } = require('../helpers/fetchProducts');
 const computadorSearch = require('../mocks/search');
 
 describe('1 - Teste a função fetchProducts', () => {
   // implemente seus testes aqui
-  fail('Teste vazio');
+  // fail('Teste vazio');
+  expect(fetchProducts).toBe('function');
 });
